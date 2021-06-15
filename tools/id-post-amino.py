4a44318a2742a11c73adfef83f35037495321438
Anna <uchihatouro@gmail.com>
2021-06-16 01:05:39 +0300

    Update id-post-amino.py

--- tools/id-post-amino.py
+++ tools/id-post-amino.py
@@ -4,5 +4,5 @@ client = amino.Client()
 client.login(email='your mail', password='your password')
 sub_client = amino.SubClient(comId='id of your community', profile=client.profile)
 
-for title, blogId in zip(sub_client.get_user_blogs('your post').title, sub_client.get_user_blogs('your post').blogId):
+for title, blogId in zip(sub_client.get_user_blogs('your account id').title, sub_client.get_user_blogs('your account id').blogId):
 	print(title, blogId)
